a73a3df521661580c63e9c085c2ec2b17f35dc9a
Kevin Tian <ktian0257@gmail.com>
2017-04-28 01:13:49 -0700

    First Commit
    Assets in, Bar and Ball classes made (but not started).
    
    Did a quick test on loading and drawing the ball's texture, works
    perfectly meaning the Content folder and ContentManager are working.
    Just need to build my classes.

---
+++ Ball.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace PongClone
+{
+    class Ball
+    {
+        #region Fields
+        private int xPos;
+        private int yPos;
+
+        public Texture2D texture;
+        #endregion
+
+        #region Constructor
+        public Ball(Texture2D texture, int xPos, int yPos)
+        {
+            this.texture = texture;
+            this.xPos = xPos;
+            this.yPos = yPos;
+        }
+
+        #endregion
+    }
+}
